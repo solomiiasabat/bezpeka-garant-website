85bb98cd987824a918477e1ea2dd4788fdf8f15f
solomiyasabat <solomiyasabat@gmail.com>
2023-03-30 21:16:56 +0300

    added google map with the location mark

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,15 @@
 export default function Footer() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1302.5677804120187!2d28.4499396128284!3d49.23591900330402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472d5c8479dad871%3A0x73f214be698efb52!2z0LLRg9C70LjRhtGPINCc0LDQs9GW0YHRgtGA0LDRgtGB0YzQutCwLCAxNTgsINCS0ZbQvdC90LjRhtGPLCDQktGW0L3QvdC40YbRjNC60LAg0L7QsdC70LDRgdGC0YwsIDIxMDAw!5e0!3m2!1suk!2sua!4v1680199799491!5m2!1suk!2sua"
+        width="600"
+        height="450"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
 }
