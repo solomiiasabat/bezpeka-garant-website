5e144c812ca59034a35073ed2c928c2551705167
solomiyasabat <solomiyasabat@gmail.com>
2023-04-01 11:04:39 +0300

    created component Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
       <Footer />
     </div>
   );
