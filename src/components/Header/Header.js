05e5b9bc37ee0c2ba73a28af19c54c9f3584baae
solomiyasabat <solomiyasabat@gmail.com>
2023-04-10 21:02:41 +0300

    added hooks and IntersectionObserver to make the contact-button move after scrolling header, this contact-button changes to phone-icon on the right side of the screen

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,51 @@
+import React, { useRef, useEffect, useState } from "react";
 import "./Header.css";
 import photo1 from "./photos/photo1.png";
 import photo2 from "./photos/photo2.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting);
+  };
+  const options = {
+    root: null,
+    rootMargin: "-5px",
+    threshold: 1.0,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunction, options);
+    if (containerRef.current) observer.observe(containerRef.current);
+
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current);
+    };
+  }, [containerRef, options]);
+
   return (
     <header>
       <div className="container">
         <div className="row">
           <div className="col mt-5 company-slogan-button">
-            <div className="row mt-5 company-name">
+            <div className="row mt-5 company-name" ref={containerRef}>
               Охоронні послуги ТОВ "Безпека-Гарант"
             </div>
             <div className="row mt-3 slogan">З нами безпечно!</div>
-            <div className="row mt-4 connect-button">
-              <button className="connect-button">Підключити</button>
-            </div>
+            {isVisible ? (
+              <button className="connect-button mt-4">Підключити</button>
+            ) : (
+              <button className="phone-icon-button mt-4">
+                <FontAwesomeIcon icon={faPhone} shake size="xl" />
+              </button>
+            )}
           </div>
+
           <div
             id="carouselExampleAutoplaying"
             className="col carousel slide"
