01ba263a9ce292e33fad48bd5152b8a28b16e0db
solomiyasabat <solomiyasabat@gmail.com>
2023-06-05 12:23:11 +0300

    added new section Security Systems, improved croll-spy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Link } from "react-scroll";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faAt } from "@fortawesome/free-solid-svg-icons";
@@ -18,7 +17,12 @@ export default function Footer() {
         <div className="row">
           <div className="col mt-4 footer-logo">
             {" "}
-            <img src="logo1.png" alt="small logo" width="80" height="80"></img>
+            <img
+              src="logo1.png"
+              alt="small logo"
+              width="100"
+              height="100"
+            ></img>
           </div>
 
           <div className="col mt-4">
@@ -33,7 +37,7 @@ export default function Footer() {
                   spy={true}
                   smooth={true}
                   duration={50}
-                  offset={-80}
+                  offset={-190}
                 >
                   Про компанію
                 </Link>
@@ -48,7 +52,7 @@ export default function Footer() {
                   spy={true}
                   smooth={true}
                   duration={50}
-                  offset={-110}
+                  offset={-200}
                 >
                   Послуги
                 </Link>
@@ -63,9 +67,24 @@ export default function Footer() {
                   spy={true}
                   smooth={true}
                   duration={50}
-                  offset={-120}
+                  offset={-210}
                 >
                   Ціни
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="nav-link"
+                  aria-current="page"
+                  onClick={openBar}
+                  activeClass="active"
+                  to="securitySystems"
+                  spy={true}
+                  smooth={true}
+                  duration={50}
+                  offset={-215}
+                >
+                  Системи безпеки
                 </Link>
               </li>
               <li>Інструкція з використання</li>
@@ -78,8 +97,8 @@ export default function Footer() {
               <li>
                 {" "}
                 <FontAwesomeIcon icon={faPhone} color={"#ffcc00"} />{" "}
-                <a href="tel:+380-067-432-02-72" className="ms-1">
-                  067 432-02-72
+                <a href="tel:+380-067-433-09-00" className="ms-1">
+                  067 433-09-00
                 </a>
               </li>
               <li>
@@ -99,9 +118,9 @@ export default function Footer() {
 
           <div className="col mt-4 mb-2 location">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5210.274222369511!2d28.446605412460453!3d49.2358896082366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472d5b7b3f65d3cb%3A0x691af8f9ff1dd84b!2z0JHQtdC30L_QtdC60LAt0JPQsNGA0LDQvdGC!5e0!3m2!1suk!2sua!4v1684843047658!5m2!1suk!2sua"
-              width="480"
-              height="300"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4962.333416197972!2d28.448239431452294!3d49.23724151580856!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472d5b7b3f65d3cb%3A0x691af8f9ff1dd84b!2z0JHQtdC30L_QtdC60LAt0JPQsNGA0LDQvdGC!5e0!3m2!1suk!2sua!4v1685084807907!5m2!1suk!2sua"
+              width="600"
+              height="380"
               style={{ border: "0" }}
               allowFullScreen=""
               loading="lazy"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,8 @@ export default function Navbar() {
   return (
     <nav className="navbar sticky-top  navbar-expand-lg">
       <div className="container-fluid">
-        <a className="navbar-brand  ms-5" href="#">
-          <img src="logo1.png" alt="Logo" width="50" height="50"></img>
+        <a className="navbar-brand  ms-5" href="/Navbar">
+          <img src="logo1.png" alt="Logo" width="80" height="80"></img>
         </a>
         <ul className="navbar-nav me-auto ms-5 mt-4 mb-3">
           <li className="nav-item me-3">
@@ -24,7 +24,7 @@ export default function Navbar() {
               spy={true}
               smooth={true}
               duration={50}
-              offset={-90}
+              offset={-230}
             >
               Про компанію
             </Link>
@@ -39,7 +39,7 @@ export default function Navbar() {
               spy={true}
               smooth={true}
               duration={50}
-              offset={-110}
+              offset={-250}
             >
               Послуги
             </Link>
@@ -54,9 +54,24 @@ export default function Navbar() {
               spy={true}
               smooth={true}
               duration={50}
-              offset={-120}
+              offset={-240}
             >
               Ціни
+            </Link>
+          </li>
+          <li className="nav-item ms-3 me-3">
+            <Link
+              className="nav-link"
+              aria-current="page"
+              onClick={openBar}
+              activeClass="active"
+              to="securitySystems"
+              spy={true}
+              smooth={true}
+              duration={50}
+              offset={-240}
+            >
+              Системи безпеки
             </Link>
           </li>
           <li className="nav-item ms-3 me-3">
@@ -69,15 +84,15 @@ export default function Navbar() {
               spy={true}
               smooth={true}
               duration={50}
-              offset={-110}
+              offset={-350}
             >
-              <a>Контакти</a>
+              Контакти
             </Link>
           </li>
         </ul>
         <span className="tel-number me-4">
-          тел.: <a href="tel:+380-067-432-02-72">067 432-02-72</a>,{" "}
-          <a href="tel:+380-097-282-46-33">097 282-46-33</a>
+          тел.: <a href="tel:+380-067-433-09-00">067 433-09-00</a>,{" "}
+          <a href="tel:+380-067-432-18-00">067 432-18-00</a>
         </span>
       </div>
     </nav>
