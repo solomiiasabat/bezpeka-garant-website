18aa3180a0a620bc737e9383c278f08360c22368
solomiyasabat <solomiyasabat@gmail.com>
2023-07-07 16:47:07 +0300

    added a POST request, made the input-form reset after submit and after closing, made max-length for input symbols, added alert when there are not enough symbols in input, added alert after submitting

--- src/components/Header/Modal/Modal.js
+++ src/components/Header/Modal/Modal.js
@@ -1,13 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function ModalMain(props) {
   const [enterNumber, setEnterNumber] = useState("");
+  const inputRef = useRef(null);
 
   const handleChange = (e) => {
     const value = e.target.value.replace(/[^+,0-9]/g, "");
     setEnterNumber(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputRef.current.value.length < 9) {
+      inputRef.current.value = "";
+      setEnterNumber(null);
+      return alert(
+        "У вашому номері не вистарчає цифр, перевірте його, будь-ласка"
+      );
+    } else {
+      axios
+        .post(
+          `https://api.telegram.org/bot6273049555:AAFihIbql9HVT6SNNe9d8TJr2YkrvdjbnbM/sendMessage?chat_id=397037025&text=${enterNumber}`
+        )
+        .then((response) => {
+          console.log(response.status, response.data.token);
+          alert(
+            "Дякуємо, що обрали Безпеку Гарант! Наш менеджер зв'яжеться з вами в найближчий час."
+          );
+        });
+    }
+
+    inputRef.current.value = "";
+    setEnterNumber("");
   };
 
   if (!props.open) return null;
@@ -26,15 +54,18 @@ export default function ModalMain(props) {
             <input
               type="phone"
               placeholder="  +380 ..."
+              maxLength="13"
               className="number-bar me-2 p-2"
               value={enterNumber}
               onChange={handleChange}
+              ref={inputRef}
             ></input>
 
             <input
               type="submit"
               className="callMe-button"
               value="Зателефонуйте мені"
+              onClick={handleSubmit}
             ></input>
           </form>
           <p className="mt-4 text-under-form">
